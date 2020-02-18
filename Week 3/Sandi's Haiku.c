6099ba0452063873808f28a93711724dab817a10
r. d. w. rome <racheldevorahstudio@gmail.com>
2020-02-18 15:42:46 -0500

    feedback on haiku

--- Week 3/Sandi's Haiku.c
+++ Week 3/Sandi's Haiku.c
@@ -4,6 +4,6 @@ int main(void)
   printf("Blissful aurora\a\n"); /*I went and saw the tutor*/
   printf("A cold, awful winter sings\a\n"); /*He helped me a lot and explained the process*/
   printf("Betrayed by the heart\a\n"); /*I was able to come up with this outcome*/
-
+/* ahh so good Sandi except! you misspelled "return!" so it doesn't compile! always triple check that something compiles!*/
   retern 0;
 }
