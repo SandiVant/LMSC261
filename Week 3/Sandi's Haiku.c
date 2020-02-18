4fbac19afe3ddb998b33addc2f197769178892fe
SandiVant <60371120+SandiVant@users.noreply.github.com>
2020-02-17 22:03:56 -0500

    3

---
+++ Week 3/Sandi's Haiku.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+  printf("Blissful aurora\a\n"); /*I went and saw the tutor*/
+  printf("A cold, awful winter sings\a\n"); /*He helped me a lot and explained the process*/
+  printf("Betrayed by the heart\a\n"); /*I was able to come up with this outcome*/
+
+  retern 0;
+}
